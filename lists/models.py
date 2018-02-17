cbe36760bcbc72d04c2f732d60aea3e4354ff440
Chris McDonald <chris.mcdonald@getbraintree.com>
2018-02-17 18:10:19 -0500

    add new list model and relate item and list models

--- lists/models.py
+++ lists/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey('list', default=None)
+
+
+class List(models.Model):
+    pass
